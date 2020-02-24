6ba4f8b74b7dcbba6d274ea8f8d8788a1bb70f8c
Hardi shah <hardis.mobio@gmail.com>
2020-02-24 11:15:58 +0530

    uploading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
+import * as $ from 'jquery';
 import { Component } from '@angular/core';
+import { setTheme } from 'ngx-bootstrap/utils';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'smart-tool';
+	constructor(){
+		setTheme('bs4'); 
+	}
+	title = 'smart-tool';
+	public ngOnInit()
+	{
+		jQuery('.svgImg').each(function($) {
+			/*  alert('test'); */
+			var $img = jQuery(this);
+			var imgID = $img.attr('id');
+			var imgClass = $img.attr('.svgImg');
+			var imgURL = $img.attr('src');
+			jQuery.get(imgURL, function(data) {
+				/* Get the SVG tag, ignore the rest */
+				var $svg = jQuery(data).find('svg');
+				/* Add replaced image's ID to the new SVG */
+				if (typeof imgID !== 'undefined') {
+					$svg = $svg.attr('id', imgID);
+				}
+				/* Add replaced image's classes to the new SVG */
+				if (typeof imgClass !== 'undefined') {
+					$svg = $svg.attr('class', imgClass + ' replaced-svg');
+				}
+				/* Remove any invalid XML tags as per http://validator.w3.org */
+				$svg = $svg.removeAttr('xmlns:a');
+				/* Replace image with new SVG*/
+				$img.replaceWith($svg);
+			}, 'xml');
+		});
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BannerComponent } from './homepage/banner/banner.component';
+import { SolutionComponent } from './homepage/solution/solution.component';
+import { HowItWorksComponent } from './homepage/how-it-works/how-it-works.component';
+import { FeaturesComponent } from './homepage/features/features.component';
+import { PricingPlansComponent } from './homepage/pricing-plans/pricing-plans.component';
+import { StartWithUsComponent } from './homepage/start-with-us/start-with-us.component';
+import { MapComponent } from './homepage/map/map.component';
+import { ContactUsComponent } from './homepage/contact-us/contact-us.component';
+import { ClientsComponent } from './homepage/clients/clients.component';
+import { VideoComponent } from './homepage/solution/video/video.component';
+import { BlogComponent } from './homepage/blog/blog.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+	AppComponent,
+	HeaderComponent,
+	FooterComponent,
+	HomepageComponent,
+	BannerComponent,
+	SolutionComponent,
+	HowItWorksComponent,
+	FeaturesComponent,
+	PricingPlansComponent,
+	StartWithUsComponent,
+	MapComponent,
+	ContactUsComponent,
+	ClientsComponent,
+	VideoComponent,
+	BlogComponent
+	],
+	imports: [
+	BrowserModule,
+	NgbModule,
+	ModalModule.forRoot()
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
